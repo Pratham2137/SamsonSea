3281b3db7f3c3d69fabcf486623272a6f4750c14
Pratham02 <prathamshinde2137@gmail.com>
2024-10-07 01:35:54 +0530

    Added Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Service from "./components/Service";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
+import Loader from "./components/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // Simulate initial loading for the entire app
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false); // After 2 seconds, remove the loader
+    }, 2000);
+  }, []);
+
+  if (loading) {
+    return <Loader />; // Show loader until the app is ready
+  }
+
   return (
     <Router>
       <Navbar />
